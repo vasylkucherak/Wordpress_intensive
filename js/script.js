321561d81ca758b5f37da72aae2db8ac7b9e021b
vasylkucherak <vasil.kucherak@gmail.com>
2022-02-22 11:59:55 +0200

    v.0.7.5 (upgraded first screan scale, links and added animation on the site)

---
+++ js/script.js
@@ -0,0 +1,31 @@
+let links = document.querySelectorAll('[href^="#"]'),
+    speed = 0.5;
+
+links.forEach(link => {
+    link.addEventListener('click', function(event) {
+        event.preventDefault();
+
+        let widthTop = document.documentElement.scrollTop,
+            hash = this.hash,
+            toBlock = document.querySelector(hash).getBoundingClientRect().top,
+            start = null;
+
+        requestAnimationFrame(step);
+
+        function step(time) {
+            if (start === null) {
+                start = time;
+            }
+
+            let progress = time - start,
+                r = (toBlock < 0 ? Math.max(widthTop - progress/speed, widthTop + toBlock) : Math.min(widthTop + progress/speed, widthTop + toBlock));
+                document.documentElement.scrollTo(0, r);
+
+            if (r != widthTop + toBlock) {
+                requestAnimationFrame(step);
+            } else {
+                location.hash = hash;
+            }
+        }
+    });
+});
